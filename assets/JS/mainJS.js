1927066e1004686342f6bb1f6ec77ac543a5acb7
Rusiru H. Dasanayaka <dasanayakarusiru@gmail.com>
2023-06-15 11:28:16 +0530

    update

--- assets/JS/mainJS.js
+++ assets/JS/mainJS.js
@@ -1,10 +1,25 @@
-function typeWriter(text) {
-    let speed=50;
-    if (index < text.length) {
-        $('#typewriter').append(text.charAt(index));
-        index++;
-        setTimeout(typeWriter, speed);
+/*
+
+function letter(string,div){
+    let s=string;
+    let ltrUse=string;
+    let index=0;
+    $(div).text(s.substring(0,index));
+    index++;
+
+    if (index===string.length+1){
+        index=0;
+        s=ltrUse;
+
     }
 }
 
-$("body>main>section:nth-child(2)>div:nth-child(1)>p:nth-child(1)").onmousemove
+
+$(document).ready(function() {
+    setInterval(letter("Hi There...","#s1p1"),100);
+   /!* setInterval(letter("I am Rusiru buuut...","#s1p2"),100);
+    setInterval( letter("You can call me","#s1p3"),100);
+    setInterval( letter("Luci.","#s1p4"),100);*!/
+
+
+});*/
