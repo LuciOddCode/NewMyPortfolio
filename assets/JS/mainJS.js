2f3bae20da9659c4ed62c7ff1faae12d13feb614
Rusiru H. Dasanayaka <dasanayakarusiru@gmail.com>
2023-06-28 11:56:46 +0530

    loader added

--- assets/JS/mainJS.js
+++ assets/JS/mainJS.js
@@ -102,3 +102,8 @@ function textAnimator(element) {
     }
 }
 
+var loader = document.getElementById("pre-loader");
+window.addEventListener("load", function(){
+    
+    loader.style.display = "none";
+});
